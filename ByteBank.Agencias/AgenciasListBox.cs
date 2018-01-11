b6e9b8eb824926cdefb415ce0ec123c50ed30a8e
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-01-11 16:25:59 -0200

    Video 1.3

---
+++ ByteBank.Agencias/AgenciasListBox.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ByteBank.Agencias
+{
+    public class AgenciasListBox : ListBox
+    {
+        private readonly MainWindow _janelaMae;
+
+        public AgenciasListBox(MainWindow janelaMae)
+        {
+            _janelaMae = janelaMae ?? throw new ArgumentNullException(nameof(janelaMae));
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+
+            var agenciaSelecionada = (Agencia)SelectedItem;
+
+            _janelaMae.txtNumero.Text = agenciaSelecionada.Numero;
+            _janelaMae.txtNome.Text = agenciaSelecionada.Nome;
+            _janelaMae.txtTelefone.Text = agenciaSelecionada.Telefone;
+            _janelaMae.txtEndereco.Text = agenciaSelecionada.Endereco;
+            _janelaMae.txtDescricao.Text = agenciaSelecionada.Descricao;
+        }
+    }
+}
--- ByteBank.Agencias/MainWindow.xaml.cs
+++ ByteBank.Agencias/MainWindow.xaml.cs
@@ -22,20 +22,47 @@ namespace ByteBank.Agencias
     public partial class MainWindow : Window
     {
         private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
+        private readonly AgenciasListBox lstAgencias;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            lstAgencias = new AgenciasListBox(this);
             AtualizarControles();
         }
 
         private void AtualizarControles()
         {
+            lstAgencias.Width = 270;
+            lstAgencias.Height = 290;
+
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
+
+            container.Children.Add(lstAgencias);
+
             lstAgencias.Items.Clear();
             var agencias = _contextoBancoDeDados.Agencias.ToList();
             foreach (var agencia in agencias)
                 lstAgencias.Items.Add(agencia);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var confirmacao = 
+                MessageBox.Show(
+                    "Você deseja realmente excluir este item?",
+                    "Confirmação",
+                    MessageBoxButton.YesNo);
+            if (confirmacao == MessageBoxResult.Yes)
+            {
+                //Excluir
+            }
+            else
+            {
+                //Não faz nada
+            }
         }
     }
 }
