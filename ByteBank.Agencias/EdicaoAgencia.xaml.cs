d8ba86a50d608ac08af595c98b672fd19bfa4852
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-01-11 17:15:40 -0200

    Video 3.6

--- ByteBank.Agencias/EdicaoAgencia.xaml.cs
+++ ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -51,23 +51,21 @@ namespace ByteBank.Agencias
             btnOk.Click += okEventHandler;
             btnCancelar.Click += cancelarEventHandler;
 
-            txtNome.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtNome);
-            txtDescricao.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtDescricao);
-            txtEndereco.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtEndereco);
-            txtNumero.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtNumero);
-            txtTelefone.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtTelefone);
+            txtNome.TextChanged += ValidarCampoNulo;
+            txtDescricao.TextChanged += ValidarCampoNulo;
+            txtEndereco.TextChanged += ValidarCampoNulo;
+            txtNumero.TextChanged += ValidarCampoNulo;
+            txtTelefone.TextChanged += ValidarCampoNulo;
         }
         
-        private TextChangedEventHandler ConstruirDelegateValidacaoCampoNulo(TextBox txt)
+        private void ValidarCampoNulo(object sender, EventArgs e)
         {
-            return (o, e) =>
-            {
-                var textoEstaVazio = String.IsNullOrEmpty(txt.Text);
+            var txt = sender as TextBox;
+            var textoEstaVazio = String.IsNullOrEmpty(txt.Text);
 
-                txt.Background = textoEstaVazio
-                    ? new SolidColorBrush(Colors.OrangeRed)
-                    : new SolidColorBrush(Colors.White);
-            };
+            txt.Background = textoEstaVazio
+                ? new SolidColorBrush(Colors.OrangeRed)
+                : new SolidColorBrush(Colors.White);
         }
 
         private void Fechar(object sender, EventArgs e) =>
