ebec3a562e43a8a2f2b0289d2a0415447788c3f6
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-01-11 17:19:20 -0200

    Video 4.5

--- ByteBank.Agencias/ValidacaoTextBox.cs
+++ ByteBank.Agencias/ValidacaoTextBox.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ByteBank.Agencias
 {
@@ -12,5 +13,22 @@ namespace ByteBank.Agencias
     public class ValidacaoTextBox : TextBox
     {
         public event ValidacaoEventHandler Validacao;
+
+        public ValidacaoTextBox()
+        {
+            TextChanged += ValidacaoTextBox_TextChanged;
+        }
+
+        private void ValidacaoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Validacao != null)
+            {
+                var ehValido = Validacao(Text);
+
+                Background = ehValido
+                    ? new SolidColorBrush(Colors.White)
+                    : new SolidColorBrush(Colors.OrangeRed);
+            }
+        }
     }
 }
