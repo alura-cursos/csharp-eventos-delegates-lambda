280ff3afa394146d41bc1a5b6fee26d389c1bafd
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-01-11 17:09:45 -0200

    Video 2.2

--- ByteBank.Agencias/MainWindow.xaml.cs
+++ ByteBank.Agencias/MainWindow.xaml.cs
@@ -22,13 +22,13 @@ namespace ByteBank.Agencias
     public partial class MainWindow : Window
     {
         private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
-        private readonly AgenciasListBox lstAgencias;
+        private readonly ListBox lstAgencias;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            lstAgencias = new AgenciasListBox(this);
+            lstAgencias = new ListBox();
             AtualizarControles();
         }
 
@@ -40,12 +40,25 @@ namespace ByteBank.Agencias
             Canvas.SetTop(lstAgencias, 15);
             Canvas.SetLeft(lstAgencias, 15);
 
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(lstAgencias_SelectionChanged);
+
             container.Children.Add(lstAgencias);
 
             lstAgencias.Items.Clear();
             var agencias = _contextoBancoDeDados.Agencias.ToList();
             foreach (var agencia in agencias)
                 lstAgencias.Items.Add(agencia);
+        }
+
+        private void lstAgencias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var agenciaSelecionada = (Agencia)lstAgencias.SelectedItem;
+
+            txtNumero.Text = agenciaSelecionada.Numero;
+            txtNome.Text = agenciaSelecionada.Nome;
+            txtTelefone.Text = agenciaSelecionada.Telefone;
+            txtEndereco.Text = agenciaSelecionada.Endereco;
+            txtDescricao.Text = agenciaSelecionada.Descricao;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
